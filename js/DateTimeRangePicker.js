d3ccb7735c5ec9d6e4948c2a615d0f4258a06743
harphield <harphield@gmail.com>
2022-06-06 14:25:07 +0200

    var fix

--- js/DateTimeRangePicker.js
+++ js/DateTimeRangePicker.js
@@ -650,7 +650,7 @@ export default class DateRangePicker {
         this.renderCalendar('right');
 
         //highlight any predefined range matching the current start and end dates
-        rangesLiElList = this.container.querySelectorAll('.ranges li');
+        var rangesLiElList = this.container.querySelectorAll('.ranges li');
         for(let i = 0; i < rangesLiElList.length; ++i)
             rangesLiElList[i].classList.remove('active');
 
